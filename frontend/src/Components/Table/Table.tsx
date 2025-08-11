08289801f8d08e50ba984b9bf3f90621b3aa61ef
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-08-11 18:36:58 -0500

    table and design guide added

---
+++ frontend/src/Components/Table/Table.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { testIncomeStatementData } from './testData';
+
+const data = testIncomeStatementData;
+
+type Props = {};
+
+type Company = (typeof data)[0];
+
+const configs = [
+  {
+    label: "Year",
+    render: (company: Company) => company.acceptedDate
+  },
+  {
+    label: "Cost of revenue",
+    render: (company: Company) => company.costOfRevenue
+  }
+]
+
+const Table = (props: Props) => {
+  const renderedRows = data.map((company) => {
+    return (
+      <tr key={company.cik}>
+        {configs.map((val: any) => {
+          return (
+            <td className='p-4 whitespace-nowrap text-sm font-normal text-gray-900'>
+              {val.render(company)}
+            </td>
+          )
+        })}
+      </tr>
+    );
+  });
+  const renderedHeaders = (
+    <tr>
+      {configs.map((config: any) => (
+        <th
+          key={config.label}
+          className="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+        >
+          {config.label}
+        </th>
+      ))}
+    </tr>
+  );
+  return <div className='bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8'>
+    <table>
+      <thead className='min-w-full divide-y divide-gray-200 m-5'>
+        {renderedHeaders}
+        <tbody>
+          {renderedRows}
+        </tbody>
+      </thead>
+    </table>
+  </div>
+};
+
+export default Table
