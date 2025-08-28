c8a8a682761d92720a73dbed24924f79adc6716a
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-08-27 23:00:53 -0500

    implemented async await

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b406eb1cc5b30c3aec50bbafae454d7cfb90b9f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d73f1871cd04e573d397087b5f7b00365381a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
