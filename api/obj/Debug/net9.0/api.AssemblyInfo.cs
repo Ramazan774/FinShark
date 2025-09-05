decbba6cd76f7e0d7da45043f5493d314dbc151d
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-09-04 22:06:05 -0500

    data validation completed

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22d9961b48b17faa90fea78d73fab57f020cfeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f4d64039788690c6eb21d019ef7b3cfb254b8b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
