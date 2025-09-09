83b0a07ed478f91408b4825b2d06a4a1dd440e48
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-09-08 21:59:36 -0500

    sorting added

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+decbba6cd76f7e0d7da45043f5493d314dbc151d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1df134b8e10dcacdbb75bc9cc6c788aeb6917b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
