8e8042698958b005f1647aa043b212d63fe31eb0
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-09-08 21:47:14 -0500

    added update

--- api/Dtos/Comment/CommentDto.cs
+++ api/Dtos/Comment/CommentDto.cs
@@ -10,9 +10,8 @@ namespace api.Dtos.Comment
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public int? StockId { get; set; }
-        
     }
     
 }
--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -10,7 +10,7 @@ namespace api.Models
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public int? StockId { get; set; }
         //Navigation property
         public Stock? Stock { get; set; }
