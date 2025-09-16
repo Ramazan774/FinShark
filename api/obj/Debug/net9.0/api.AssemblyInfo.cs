c62c0ef221e7a1ced4c69aa577341ba7a3b4e1bc
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-09-15 20:50:31 -0500

    created first token

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e03d33b541f41083257e29b02522c922c5bb2762")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f4354ec9ad4aa29ebb9a6423303c83268a480f")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
