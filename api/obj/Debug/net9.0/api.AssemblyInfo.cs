b9b996649ec2ff81582a14b220b52ed4f6900722
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-09-21 20:49:24 -0500

    new ifmp

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a18ad8c7b46a854c5df2f5d7ad6044a0f1de3aeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c475c01a887236daabf483e731960793358683")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
