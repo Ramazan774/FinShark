17c0a3f96ef4db18057cd9bec08014de8e22a4f5
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-09-30 19:02:19 -0500

    fixed comment issue

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+926889fd769b1aaa54417c39ce7dc4fbe0aaa3c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be54370544c9b89ff2f81d226e069665e0c02fc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
